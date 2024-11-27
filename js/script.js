62435917f6fa83eb2696df799fb89a8fbac0000d
Rimy Akter <rimy@gmail.com>
2024-11-27 21:30:59 +0600

    Added counter-up file to remote

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+    //counter up
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1500
+    });
+    //Progress Bar
+    if($('.progress-line').length) {
+        $('.progress-line').appear(function(){
+            var el = $(this);
+            var percent = el.data('width');
+            $(el).css('width', percent+'%');
+        }, {accY: 0});
+       }
+});
